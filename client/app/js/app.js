b5ad6ba5ad342ca38a1b263b3b4c3e57af8b5a1a
Nick Portokallidis <portokallidis@gmail.com>
2015-09-10 09:37:02 +0000

    clean up and finalize release

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -10,9 +10,7 @@
 angular.module('loopbackApp', [
     'angular-loading-bar',
     'angular.filter',
-    'angularBootstrapNavTree',
     'angularFileUpload',
-    // 'btford.markdown',
     'oitozero.ngSweetAlert',
     'config',
     'formly',
@@ -24,18 +22,12 @@ angular.module('loopbackApp', [
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngMap',
     'ngVis',
     'ui.bootstrap',
-    'ui.codemirror',
     'ui.gravatar',
-    'ui.grid',
     'ui.router',
     'toasty',
-    'autofields',
-    'gettext',
     'angular-underscore/filters',
-    'schemaForm',
     'ui.select',
     "angular-advanced-searchbox",
     'com.module.core',
@@ -46,58 +38,3 @@ angular.module('loopbackApp', [
     'com.module.users',
     'permission'
   ]);
-  // .run(function($rootScope, $cookies, gettextCatalog) {
-
-  //   $rootScope.locales = {
-
-  //     'de': {
-  //       lang: 'de',
-  //       country: 'DE',
-  //       name: gettextCatalog.getString('German')
-  //     },
-  //     'en': {
-  //       lang: 'en',
-  //       country: 'US',
-  //       name: gettextCatalog.getString('English')
-  //     },
-  //     'es_MX': {
-  //       lang: 'es_MX',
-  //       country: 'MX',
-  //       name: gettextCatalog.getString('Spanish')
-  //     },
-  //     'fr': {
-  //       lang: 'fr',
-  //       country: 'FR',
-  //       name: gettextCatalog.getString('Français')
-  //     },
-  //     'nl': {
-  //       lang: 'nl',
-  //       country: 'NL',
-  //       name: gettextCatalog.getString('Dutch')
-  //     },
-  //     'pt-BR': {
-  //       lang: 'pt_BR',
-  //       country: 'BR',
-  //       name: gettextCatalog.getString('Portuguese Brazil')
-  //     },
-  //     'ru_RU': {
-  //       lang: 'ru_RU',
-  //       country: 'RU',
-  //       name: gettextCatalog.getString('Russian')
-  //     }
-  //   }
-
-  //   var lang = $cookies.lang || navigator.language || navigator.userLanguage;
-
-  //   $rootScope.locale = $rootScope.locales[lang];
-
-  //   if ($rootScope.locale === undefined) {
-  //     $rootScope.locale = $rootScope.locales[lang];
-  //     if ($rootScope.locale === undefined) {
-  //       $rootScope.locale = $rootScope.locales['en'];
-  //     }
-  //   }
-
-  //   gettextCatalog.setCurrentLanguage($rootScope.locale.lang);
-
-  // });
